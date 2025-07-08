2598e3eb87aff5ccfdc4c50df005b9b6268e68ca
akshar51 <aksharparekh401@gmail.com>
2025-07-08 16:55:00 +0530

    done

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,45 +1,61 @@
-import React from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../firebase/config";
 
 const SignUp = () => {
+
+    const [user, setUser] = useState({});
+    
+    const handleChange = (e)=>{
+        let {name,value} = e.target;
+        setUser({...user,[name]:value})
+    }
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        createUserWithEmailAndPassword(auth,email,password)
+        .then(()=>{
+            console.log("sign-up successful")
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+        setUser({})
+    }
+
   return (
     <>
       <div className="container">
         <div className="row">
           <div className="col-md-6 mx-auto mt-5">
-            <form>
+            <h1>Sign-Up</h1>
+            <form method="post" onSubmit={handleSubmit}>
               <div className="mb-3">
                 <label htmlFor="exampleInputEmail1" className="form-label">
-                  Email address
+                  Email :
                 </label>
                 <input
                   type="email"
+                  name="email"
+                  value={user.email || ''}
+                  onChange = {handleChange}
                   className="form-control"
                   id="exampleInputEmail1"
                   aria-describedby="emailHelp"
                 />
-                <div id="emailHelp" className="form-text">
-                  We'll never share your email with anyone else.
-                </div>
               </div>
               <div className="mb-3">
                 <label htmlFor="exampleInputPassword1" className="form-label">
-                  Password
+                  Password :
                 </label>
                 <input
+                  name="password"
+                  value={user.password || ''}
+                  onChange = {handleChange}
                   type="password"
                   className="form-control"
                   id="exampleInputPassword1"
                 />
-              </div>
-              <div className="mb-3 form-check">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="exampleCheck1"
-                />
-                <label className="form-check-label" htmlFor="exampleCheck1">
-                  Check me out
-                </label>
               </div>
               <button type="submit" className="btn btn-primary">
                 Submit
